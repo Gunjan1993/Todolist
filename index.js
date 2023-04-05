bba4d698b4bde068e714cf5af020aaba336cc2b8
Gunjan1993 <72492276+Gunjan1993@users.noreply.github.com>
2023-04-06 00:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/todo");
+//const mongoose=require("mongoose");
+//mongoose.connect("mongodb://127.0.0.1:27017/todo");
 
 const trySchema=new mongoose.Schema({
     name:String
